f1108bfad8da8f60f172e8490bae9b16a9c4151c
Alfredo GM <gonzalez.mendez@hotmail.com>
2025-02-09 23:19:50 -0600

    modificacion

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Cargar los datos
 car_data = pd.read_csv("vehicles_us.csv")
 
-# Limpiar Datos
 car_data['model_year'] = car_data['model_year'].fillna(car_data['model_year'].mean()).astype(int)
 car_data['model'] = car_data['model'].str.upper()
 car_data['condition'] = car_data['condition'].str.upper().fillna('UNKNOWN').astype('category')
@@ -19,22 +17,19 @@ car_data['date_posted'] = pd.to_datetime(car_data['date_posted'])
 car_data['is_4wd'] = car_data['is_4wd'].fillna('0').astype('int')
 car_data.columns = car_data.columns.str.upper()
 
-# Crear la interfaz de la aplicación
 st.title("Cuadro de Mandos de Vehículos")
 st.header("Análisis de Datos de Vehículos en Venta")
 st.header("Base de Datos:")
 st.write(car_data)
 
-# Botón para mostrar histograma de precios
-if st.button("Mostrar Histograma de Precios"):
-    fig = px.histogram(car_data, x="PRICE", title="Distribución de Precios de Vehículos")
+if st.button('Generar Histograma de Precios y Estado'):
+    fig = px.histogram(car_data, x='CONDITION, y= 'PRICE', title="Histograma de Estado y Precios de Vehículos")
     st.plotly_chart(fig)
+
+
+if st.button('Generar Gráfico de Barras de Modelos por Año'):
+    long_format_data = car_data.groupby(['MODEL_YEAR', 'MODEL']).size().reset_index(name='Count')
     
-st.write("Tabla de dispersión entre las variables numéricas:")
-
-# Seleccionar columnas numéricas para la tabla de dispersión
-numeric_columns = car_data.select_dtypes(include='number').columns.tolist()
-
-# Crear un gráfico de dispersión usando plotly_express
-scatter_matrix = px.scatter_matrix(car_data, dimensions=numeric_columns, title="Matriz de Dispersión")
-st.plotly_chart(scatter_matrix)    
+    fig = px.bar(long_format_data, x='MODEL_YEAR', y='Count', color='MODEL', 
+                 title="Distribución de Modelos por Año", labels={'Count': 'Número de Vehículos'})
+    st.plotly_chart(fig)
