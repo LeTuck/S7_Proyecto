dc8bb62feb3b468b6cba0a3211344d306bb592c0
Alfredo GM <gonzalez.mendez@hotmail.com>
2025-02-10 00:52:46 -0600

    modificacion

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 car_data = pd.read_csv("vehicles_us.csv")
+st.markdown("<title>Proyecto S7 - Alfredo Gonzalez Mendez</title>", unsafe_allow_html=True)
 
 car_data['model_year'] = car_data['model_year'].fillna(car_data['model_year'].mean()).astype(int)
 car_data['model'] = car_data['model'].str.upper()
@@ -18,9 +19,7 @@ car_data['is_4wd'] = car_data['is_4wd'].fillna('0').astype('int')
 car_data.columns = car_data.columns.str.upper()
 
 st.title("Venta de Vehículos")
-st.header("Análisis de Datos de Vehículos en Venta")
 st.header("Escoge el tipo de grafica")
-
 
 if st.button('Condicion de vehiculos'):
     long_format_data = car_data.groupby(['CONDITION', 'TYPE']).size().reset_index(name='Numero de Vehiculos')
@@ -28,6 +27,8 @@ if st.button('Condicion de vehiculos'):
     fig = px.bar(long_format_data, x='CONDITION', y='Numero de Vehiculos', color='TYPE', 
                  title="Condicion de los vehiculos")
     st.plotly_chart(fig)
+    
+st.header("o")
 
 if st.button('Modelo y Precio'):
     long_format_data = car_data.groupby(['PRICE', 'MODEL']).size().reset_index(name='Numero de Vehiculos')
