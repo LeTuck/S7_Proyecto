968227ce1ab7732257d1d6b3867152779ce93b49
Alfredo GM <gonzalez.mendez@hotmail.com>
2025-02-09 21:42:49 -0600

    modificacion

--- app.py
+++ app.py
@@ -2,18 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import scipy.stats
-import nbformat
-from nbconvert import PythonExporter
-
-notebook_path = "S7_Proyecto/notebooks/EDA.ipynb"
-with open(notebook_path, encoding="utf-8") as f:
-    nb = nbformat.read(f, as_version=4)
-
-exporter = PythonExporter()
-python_code, _ = exporter.from_notebook_node(nb)
-
-exec(python_code)
-
 
 st.header('Datos de Vehiculos')
 
